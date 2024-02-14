6e6cc761708886bfc3d11bbdaa010d1c317dc636
DOTHMANE <delassi.othmane03@gmail.com>
2024-02-14 16:27:50 +0100

    add games_controller and routes

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,13 @@
+class GamesController < ApplicationController
+  # POST /games
+  def create
+  end
+
+  # POST /games/:id/roll
+  def roll
+  end
+
+  # GET /games/:id/score
+  def score
+  end
+end
